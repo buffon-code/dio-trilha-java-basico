9603c7c1a0fda972f1a6e899d450284b6ab7584b
buffon-code <fbfranciane@gmail.com>
2024-05-11 21:59:52 -0300

    Atualizando dados

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -1,5 +1,64 @@
+
+import java.util.Scanner;
+
+
+
 public class contaTerminal{
+
+    public static void limparTela (){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá Mundo!");
+
+
+        System.out.println ("----------------Sistema Bancário------------------");
+        System.out.println ("--------------------------------------------------");
+        
+
+        Scanner inputScanner = new Scanner (System.in);
+
+        System.out.println("Por favor digite o número da Agência: ");
+        int numeroAgencia= inputScanner.nextInt();
+
+     
+        limparTela();
+
+        System.out.println("Digite o número da Conta: ");
+        int numeroConta= inputScanner.nextInt();
+
+        limparTela();
+
+        inputScanner.nextLine();
+
+        System.out.println("Digite seu nome completo: ");
+        String nome= inputScanner.nextLine(); 
+        while (!nome.matches("[a-zA-Z\\s]+")) {
+            System.out.println("Por favor, digite apenas letras. Tente novamente: ");
+            nome = inputScanner.nextLine();
+        }
+
+        limparTela();
+        System.out.println("Digite o valor: ");
+        String valorString= inputScanner.nextLine();
+
+        valorString = valorString.replace(',', '.');
+
+        // Converte a string para double
+        double valor = Double.parseDouble(valorString);
+
+        limparTela();
+
+        System.out.printf("Olá [%s], obrigado por criar uma conta em nosso banco, sua agência é: [%d] Conta: [%d] e seu saldo de: [%.2f] já está disponível para saque :)%n", nome, numeroAgencia, numeroConta, valor);
+        System.out.println();
+        System.out.println();
+        System.out.println("               Agradecemos a preferência!               ");
+        
+        
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
     }
 }
